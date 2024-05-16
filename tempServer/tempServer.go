a9ff37f8cea2e2f4ab388627fe3fa669a2b943f3
Rika <2293840045@qq.com>
2024-05-16 17:16:08 +0800

    调试用服务器载入模版

--- tempServer/tempServer.go
+++ tempServer/tempServer.go
@@ -1,10 +1,39 @@
 package tempServer
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var Templates = make(map[string]*template.Template)
+
+func init() {
+	loadAllTemplate("template")
+}
+
+// 加载一个目录下的全部模版，模版在 root 文件夹中的相对路径作为 key 存在 Templates 变量中。
+func loadAllTemplate(root string) {
+	_ = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		read, _ := os.ReadFile(path)
+		key := path[len(root)+1:]
+		Templates[key] = template.Must(template.New(path).Parse(string(read)))
+		return nil
+	})
+}
 
 func Start() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+	http.HandleFunc("/templates", func(w http.ResponseWriter, r *http.Request) {
+		for s := range Templates {
+			w.Write([]byte(s + "\n"))
+		}
 	})
 	_ = http.ListenAndServe(":8080", nil)
 }
