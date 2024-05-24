3b350b80ef487a51911c9766fbf599e8527db959
jianing.zhang <zjnguge@gmail.com>
2024-05-24 11:55:15 +0800

    增加MD读取去除空行逻辑

--- TOCGenerator/FileHandle.go
+++ TOCGenerator/FileHandle.go
@@ -4,6 +4,7 @@ import (
 	"github.com/RellwNote/RellwNote/log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // GetSummaryFileToByte 打开目录文件，获取其字节
@@ -23,6 +24,7 @@ func GetSummaryFileToByte(filePath string, fileName string) []byte {
 	if err != nil {
 		log.Error.Println("读取目录文件失败,", err)
 	}
+	content = removeEmptyLinesFromFile(content)
 	return content
 }
 
@@ -32,4 +34,20 @@ func WriteContentToFile(filePath string, content []byte) {
 	if err != nil {
 		log.Error.Println("写入文件", filePath, "失败")
 	}
+}
+
+func removeEmptyLinesFromFile(content []byte) []byte {
+	lines := strings.Split(string(content), "\n")
+	var nonEmptyLines []string
+
+	// 去除空行
+	for _, line := range lines {
+		if strings.TrimSpace(line) != "" {
+			nonEmptyLines = append(nonEmptyLines, line)
+		}
+	}
+
+	// 重新组合非空行
+	result := strings.Join(nonEmptyLines, "\n")
+	return []byte(result)
 }
